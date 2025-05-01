85c495a0dec1a619d008791ba0b337d689fd4e7b
roy__akash <akash.oy@codecraft.com>
2025-05-01 19:48:01 +0530

    Added a new app

---
+++ chaidjango/chaidjango/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hello, World!")
+    return render(request, 'website/index.html')
+
+def about(request):
+    return HttpResponse("About Page")
+
+def contact(request):
+    return HttpResponse("Contact Page")
